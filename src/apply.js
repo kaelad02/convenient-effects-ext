598a4ede885fa0f3896ed2f382e9d25fb070f305
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-03-17 14:14:05 -0700

    remove sample code

--- src/apply.js
+++ src/apply.js
@@ -18,21 +18,3 @@ export default function addApplyButton(app, html) {
 function applyConvenientEffect() {
   game.dfreds.effectInterface.toggleEffect("Bane");
 }
-
-/*
-const roll = await item.roll({configureDialog: false});
-if(!roll) return;
-
-let html = $(roll.data.content);
-let buttons = html.find(".card-buttons")[0];
-
-const actorDC = actor.data.data.attributes.spelldc;
-const newButtons = [
-    $(`<button data-action="save" data-ability="dex">Saving Throw DC ${actorDC} Dexterity (Prone)</button>`)[0],
-    $(`<button data-action="save" data-ability="str">Saving Throw DC ${actorDC} Strength (Pushed 15ft)</button>`)[0]
-];
-
-buttons.append(...newButtons);
-
-await roll.update({'content': html.prop("outerHTML")});
-*/
